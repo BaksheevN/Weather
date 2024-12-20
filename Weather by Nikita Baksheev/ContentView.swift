5029614b19b53f8be1df85f293b7e9ecf540e5c1
Nikita Baksheev <nikiba@rarus.ru>
2024-12-20 14:58:50 -0800

    Initial Commit

---
+++ Weather by Nikita Baksheev/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather by Nikita Baksheev
+//
+//  Created by Nikita Baksheev on 20.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather by Nikita Baksheev/Weather_by_Nikita_BaksheevApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_by_Nikita_BaksheevApp.swift
+//  Weather by Nikita Baksheev
+//
+//  Created by Nikita Baksheev on 20.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_by_Nikita_BaksheevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
